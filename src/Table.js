009b845122a31f8ba49a41ede5da52b1322fce2b
pmadhureddy <pmadhureddy1510@gmail.com>
2020-10-29 17:43:12 +0530

    Added bg color

--- src/Table.js
+++ src/Table.js
@@ -3,10 +3,18 @@ import "./Table.css";
 import numeral from "numeral";
 
 function Table({ countries }) {
+  console.log(countries);
   return (
     <div className="table">
-      {countries.map(({ country, cases }) => (
+      {countries.map(({ country, cases, countryInfo }) => (
         <tr>
+          <td>
+            <img
+              style={{ width: "30px", height: "30px", objectFit: "contain" }}
+              src={countryInfo.flag}
+              alt=""
+            />
+          </td>
           <td>{country}</td>
           <td>
             <strong>{numeral(cases).format("0,0")}</strong>
